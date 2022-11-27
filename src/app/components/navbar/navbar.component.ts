7d799c1698c6d439a0577992684e4523e158b4f4
Maks.Szokalski <42069493+illunix@users.noreply.github.com>
2022-11-27 10:59:41 +0100

    init

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  constructor(public router: Router) { }
+
+  onOpenPortoflio() {
+    this.openExternalUrl('https://github.com/illunix?tab=repositories');
+  }
+
+  onOpenLinkedIn() {
+    this.openExternalUrl('https://www.linkedin.com/in/maks-szokalski-255616222/');
+  }
+
+
+  onGetInTouch() {
+    window.open('mailto:maks.szokalski@icloud.com?subject=');
+  }
+
+  private openExternalUrl(url: string) {
+    (window as any).open(url, "_blank");
+  }
+}
